f01cc3b5e6371baaaa14bf3eff5d154d3aff176e
navneetranjan <navneet.ranjan014@gmail.com>
2022-04-07 01:22:51 +0530

    Beutify login page

--- frontend/my-app/src/Login.js
+++ frontend/my-app/src/Login.js
@@ -6,198 +6,226 @@ import axios from 'axios'
 import './Login.css'
 import ReactDOM from 'react-dom';
 import { Button } from 'react-bootstrap'
-import {BrowserRouter,Link,Route,Router,useHistory,Routes} from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css';
-var r=[];
-var user='',userId=''
+import { BrowserRouter, Link, Route, Router, useHistory, Routes } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import Header from './components/Header'
+var r = [];
+var user = '', userId = ''
 const Login = () => {
-    const[loginData,setLoginData]=useState({email:'',password:''})
-    const[regData,setRegData]=useState({regEmail:'',regPassword:'',regName:''})
-   
-    const[OwnerLoginData,setOwnerLoginData]=useState({email:'',password:''})
-    const[StoreRegData,setStoreRegData]=useState({regEmail:'',regPassword:'',regName:''})
+  const [loginData, setLoginData] = useState({ email: '', password: '' })
+  const [regData, setRegData] = useState({ regEmail: '', regPassword: '', regName: '' })
+
+  const [OwnerLoginData, setOwnerLoginData] = useState({ email: '', password: '' })
+  const [StoreRegData, setStoreRegData] = useState({ regEmail: '', regPassword: '', regName: '' })
 
 
-    const [n,setName]=useState(" ")
-    const [customer,setCustomer]=useState(1)
-    
-    
-      const getData=(e)=>{
-        const {name,value}=e.target;
-        setLoginData((data)=>({
-          ...data,[name]:value
-        }))
-         console.log(loginData)
-  }
+  const [n, setName] = useState(" ")
+  const [customer, setCustomer] = useState(1)
 
-  const getOwnerData=(e)=>{
-    const {name,value}=e.target;
-    setOwnerLoginData((data)=>({
-      ...data,[name]:value
+
+  const getData = (e) => {
+    const { name, value } = e.target;
+    setLoginData((data) => ({
+      ...data, [name]: value
     }))
-     console.log(OwnerLoginData)
+    console.log(loginData)
   }
 
-  const getRegData=(e)=>{
-    const {name,value}=e.target;
-    setRegData((data)=>({
-      ...data,[name]:value
+  const getOwnerData = (e) => {
+    const { name, value } = e.target;
+    setOwnerLoginData((data) => ({
+      ...data, [name]: value
+    }))
+    console.log(OwnerLoginData)
+  }
+
+  const getRegData = (e) => {
+    const { name, value } = e.target;
+    setRegData((data) => ({
+      ...data, [name]: value
     }))
     console.log(regData)
   }
-  const getStoreRegData=(e)=>{
-    const {name,value}=e.target;
-    setStoreRegData((data)=>({
-      ...data,[name]:value
+  const getStoreRegData = (e) => {
+    const { name, value } = e.target;
+    setStoreRegData((data) => ({
+      ...data, [name]: value
     }))
     console.log(StoreRegData)
   }
-  const login=async ()=>{
+  const login = async () => {
     console.log(loginData)
-    await axios.post('http://localhost:7000/userLoginConf',{loginData})
-    .then((res)=>{
-      console.log(res.data._id)
-      user=res.data.Name
-        
-      userId=res.data._id
-      
-      ReactDOM.render( 
-        
-              <App user={res.data.Name} userId={res.data._id} />
-              
-    ,document.getElementById('root'))
-    })
-  }
-  const ownerLogin=async ()=>{
-    console.log(loginData)
-    await axios.post('http://localhost:7000/OwnerLoginConf',{OwnerLoginData})
-    .then((res)=>{
-      console.log(res.data._id)
-      user=res.data.Name
-        
-      userId=res.data._id
-      
-      ReactDOM.render( 
-        
-              <OwnerApp user={res.data.Name} userId={res.data._id} />
-              
-    ,document.getElementById('root'))
-    })
-  }
-const getCustomerLogin=()=>{
-  setCustomer(1)
-}
-  const register=async ()=>{
-    console.log("clicked reg button")
-      await axios.post("http://localhost:7000/userRegistration",{regData})
-      .then((res)=>{
-         console.log(res)
+    await axios.post('http://localhost:7000/userLoginConf', { loginData })
+      .then((res) => {
+        console.log(res.data._id)
+        user = res.data.Name
+
+        userId = res.data._id
+
+        ReactDOM.render(
+
+          <App user={res.data.Name} userId={res.data._id} />
+
+          , document.getElementById('root'))
       })
   }
-  const registerOwner=async ()=>{
-    console.log("clicked reg button")
-      await axios.post("http://localhost:7000/OwnerRegistration",{StoreRegData})
-      .then((res)=>{
-         console.log(res)
+  const ownerLogin = async () => {
+    console.log(loginData)
+    await axios.post('http://localhost:7000/OwnerLoginConf', { OwnerLoginData })
+      .then((res) => {
+        console.log(res.data._id)
+        user = res.data.Name
+
+        userId = res.data._id
+
+        ReactDOM.render(
+
+          <OwnerApp user={res.data.Name} userId={res.data._id} />
+
+          , document.getElementById('root'))
       })
+  }
+  const getCustomerLogin = () => {
+    setCustomer(1)
+  }
+  const register = async () => {
+    console.log("clicked reg button")
+    await axios.post("http://localhost:7000/userRegistration", { regData })
+      .then((res) => {
+        console.log(res)
+      })
+  }
+  const registerOwner = async () => {
+    console.log("clicked reg button")
+    await axios.post("http://localhost:7000/OwnerRegistration", { StoreRegData })
+      .then((res) => {
+        console.log(res)
+      })
+  }
+  const custo = (e)=>{
+    let e1=document.getElementById("c");
+    let e2=document.getElementById("s");
+    e1.style.display='block';
+    e2.style.display='none';
+    let b1=document.getElementById("cust");
+    let b2=document.getElementById("str");
+    b1.style.backgroundColor='rgb(255 255 255)';
+    b2.style.backgroundColor='#FF4742';
+    b1.style.color = '#FF4742';
+    b2.style.color ='rgb(255 255 255)';
+  }
+  const own =(e)=>{
+    let e1=document.getElementById("c");
+    let e2=document.getElementById("s");
+    e1.style.display='none';
+    e2.style.display='block';
+    let b1=document.getElementById("cust");
+    let b2=document.getElementById("str");
+    b2.style.backgroundColor='rgb(255 255 255)';
+    b1.style.backgroundColor='#FF4742';
+    b2.style.color = '#FF4742';
+    b1.style.color ='rgb(255 255 255)';
   }
   return (<>
-  <div className='main'>
-  <div className='customer container1 col-md-8 mx-auto mt-5 mb-5'>
-     <h3><strong>Customer</strong></h3>
+  <Header user={undefined} userId={undefined}/>
+    <div className='main'>
+      <div className='p'>
+      <button id='cust' className='button-24 b' onClick={custo}>Customer</button>
+      <button id='str' className='button-24 b' onClick={own}>Store Owner</button>
+      </div>
+      <div className='customer col-md-8 mx-auto' id='c'>
+        <h3><strong>Customer</strong></h3>
         <div className='cont col-md-8 mx-auto'>
-          
+
           <div className="login">
 
             <div className='col-md-8 mx-auto'>
 
               <div className="form-outline mb-4">
-                <input placeholder='E-mail' name='email' value={loginData.email} onChange={(e)=>getData(e)} type="email" id="form2Example1" className="col-sm-2 form-control" />
+                <input placeholder='E-mail' name='email' value={loginData.email} onChange={(e) => getData(e)} type="email" id="form2Example1" className="col-sm-2 form-control" />
                 {/* <label className="form-label" for="form2Example1">Email address</label> */}
               </div>
 
               <div className="form-outline mb-4">
-                <input placeholder='Password' name='password' value={loginData.password} onChange={(e)=>getData(e)} type="password" id="form2Example2" className="form-control" />
+                <input placeholder='Password' name='password' value={loginData.password} onChange={(e) => getData(e)} type="password" id="form2Example2" className="form-control" />
                 {/* <label class="form-label" for="form2Example2">Password</label> */}
               </div>
               <div className="form-outline mb-4">
-                
-                <Button className="btn-dark" onClick={login}>Login</Button>
+
+                <button className="button-24" onClick={login}>Login</button>
               </div>
-                
-             </div>
-              </div>
-            
-             
-              </div>
-              <div className='register col-md-8 mx-auto mb-2'>
-              <div className='col-md-8 mx-auto'>
-              <div className="form-outline mb-4">
-                <input placeholder='Email' name="regEmail" value={regData.regEmail} onChange={getRegData} type="email" id="form2Example1" className="col-sm-2 form-control" />
-                {/* <label className="form-label" for="form2Example1">Email address</label> */}
-              </div>
-              <div className="form-outline mb-4">
-                <input placeholder='Name'  name='regName' value={regData.regName} onChange={getRegData} type="email" id="form2Example1" className="col-sm-2 form-control" />
-                {/* <label className="form-label" for="form2Example1">Email address</label> */}
-              </div>
-              <div className="form-outline mb-4">
-                <input placeholder='Password'  name='regPassword' value={regData.regPassword} onChange={getRegData} type="email" id="form2Example1" className="col-sm-2 form-control" />
-                {/* <label className="form-label" for="form2Example1">Email address</label> */}
-              </div>
-                <Button className="btn-dark" onClick={register}>Register</Button>
-                </div>
-              
-              </div>
-             
-             
-  </div>  
-  <div className='storeOwner col-md-8 mx-auto'>
-   <h3><strong>Store Owners</strong></h3>
-  <div className='cont col-md-8 mx-auto'>
-           
-           <div className="login col-md-8 mx-auto">
-           <div className="form-outline mb-4">
-                <input placeholder='E-mail' name='email' value={OwnerLoginData.email} onChange={(e)=>getOwnerData(e)} type="email" id="form2Example1" className="col-sm-2 form-control" />
-                {/* <label className="form-label" for="form2Example1">Email address</label> */}
-              </div>
-              <div className="form-outline mb-4">
-                <input placeholder='Password' name='password' value={OwnerLoginData.password} onChange={(e)=>getOwnerData(e)}  type="email" id="form2Example1" className="col-sm-2 form-control" />
-                {/* <label className="form-label" for="form2Example1">Email address</label> */}
-              </div>
-              
-              <Button className="btn-dark"  onClick={ownerLogin}>Login</Button>
-              
-           
+
             </div>
-            
-             </div>
-             <div className='register col-md-8 mx-auto mt-3 mb-3'>
-               <div className="col-md-8 mx-auto">
-             <div className="form-outline mb-4">
-                <input placeholder='Email' name="regEmail" value={StoreRegData.regEmail} onChange={getStoreRegData} type="email" id="form2Example1" className="col-sm-2 form-control" />
-                {/* <label className="form-label" for="form2Example1">Email address</label> */}
-              </div>
-              <div className="form-outline mb-4">
-                <input placeholder='Name'  name='regName' value={StoreRegData.regName} onChange={getStoreRegData} type="email" id="form2Example1" className="col-sm-2 form-control" />
-                {/* <label className="form-label" for="form2Example1">Email address</label> */}
-              </div>
-              <div className="form-outline mb-4">
-                <input placeholder='Password'  name='regPassword' value={StoreRegData.regPassword} onChange={getStoreRegData} type="email" id="form2Example1" className="col-sm-2 form-control" />
-                {/* <label className="form-label" for="form2Example1">Email address</label> */}
-              </div>
-               
-               <Button  className="btn-dark" onClick={registerOwner} >Register</Button>
-               </div>
-             </div>
-    </div>         
-             
+          </div>
+        </div>
+        
+        <div className='register col-md-8 mx-auto mb-2'>
+          <div className='col-md-8 mx-auto'>
+            <div className="form-outline mb-4">
+              <input placeholder='Email' name="regEmail" value={regData.regEmail} onChange={getRegData} type="email" id="form2Example1" className="col-sm-2 form-control" />
+              {/* <label className="form-label" for="form2Example1">Email address</label> */}
+            </div>
+            <div className="form-outline mb-4">
+              <input placeholder='Name' name='regName' value={regData.regName} onChange={getRegData} type="email" id="form2Example1" className="col-sm-2 form-control" />
+              {/* <label className="form-label" for="form2Example1">Email address</label> */}
+            </div>
+            <div className="form-outline mb-4">
+              <input placeholder='Password' name='regPassword' value={regData.regPassword} onChange={getRegData} type="email" id="form2Example1" className="col-sm-2 form-control" />
+              {/* <label className="form-label" for="form2Example1">Email address</label> */}
+            </div>
+            <button className="button-24" onClick={register}>Register</button>
+          </div>
+
+        </div>
+      </div>
+
+      <div className='storeOwner col-md-8 mx-auto' id='s'>
+        <h3><strong>Store Owners</strong></h3>
+        <div className='cont col-md-8 mx-auto'>
+
+          <div className="login col-md-8 mx-auto">
+            <div className="form-outline mb-4">
+              <input placeholder='E-mail' name='email' value={OwnerLoginData.email} onChange={(e) => getOwnerData(e)} type="email" id="form2Example1" className="col-sm-2 form-control" />
+              {/* <label className="form-label" for="form2Example1">Email address</label> */}
+            </div>
+            <div className="form-outline mb-4">
+              <input placeholder='Password' name='password' value={OwnerLoginData.password} onChange={(e) => getOwnerData(e)}  type="email" id="form2Example1" className="col-sm-2 form-control" />
+              {/* <label className="form-label" for="form2Example1">Email address</label> */}
+            </div>
+
+            <button className="button-24" onClick={ownerLogin}>Login</button>
+
+
+          </div>
+
+        </div>
+        <div className='register col-md-8 mx-auto mt-3 mb-3'>
+          <div className="col-md-8 mx-auto">
+            <div className="form-outline mb-4">
+              <input placeholder='Email' name="regEmail" value={StoreRegData.regEmail} onChange={getStoreRegData} type="email" id="form2Example1" className="col-sm-2 form-control" />
+              {/* <label className="form-label" for="form2Example1">Email address</label> */}
+            </div>
+            <div className="form-outline mb-4">
+              <input placeholder='Name' name='regName' value={StoreRegData.regName} onChange={getStoreRegData} type="email" id="form2Example1" className="col-sm-2 form-control" />
+              {/* <label className="form-label" for="form2Example1">Email address</label> */}
+            </div>
+            <div className="form-outline mb-4">
+              <input placeholder='Password' name='regPassword' value={StoreRegData.regPassword} onChange={getStoreRegData} type="email" id="form2Example1" className="col-sm-2 form-control" />
+              {/* <label className="form-label" for="form2Example1">Email address</label> */}
+            </div>
+
+            <button className="button-24" onClick={registerOwner} >Register</button>
+          </div>
+        </div>
+      </div>
+
     </div>
-              </>
-    )
+  </>
+  )
 }
 
 export default Login
-export {user,userId}
+export { user, userId }
 
 
 
